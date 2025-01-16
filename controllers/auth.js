ed212f7815558b7d9fbe5d5fbb2a78ee77535b05
Darío Rojas <drojas@makingsense.com>
2025-01-16 14:53:52 -0300

    [Cookies-Session] This is the first approach with sessions, later on i will start implementing auth

---
+++ controllers/auth.js
@@ -0,0 +1,27 @@
+const User = require("../models/user");
+
+exports.getLogin = (req, res, next) => {
+  //   const isLoggedIn = req.get("Cookie").split(";")[2].trim().split("=")[1];
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "Login",
+    isAuthenticated: req.session.isLoggedIn,
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  User.findById("678808d7ae6c9082307fc0b4")
+    .then((user) => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      res.redirect("/");
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy((err) => {
+    console.log(err);
+    res.redirect("/");
+  });
+};
