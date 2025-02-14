9f79ff2ec1d96c296d920028b76b1d293163ce27
Dario Rojas <drojas@makingsense.com>
2025-02-14 18:05:42 -0300

    [Delete files] Add file deletion

--- controllers/admin.js
+++ controllers/admin.js
@@ -187,24 +187,22 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
-  Product.findById(req.body.productId)
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId)
     .then((product) => {
       if (!product) {
-        return next(new Error("Product not found"));
+        return next(new Error("Product not found."));
       }
       fileHelper.deleteFile(product.imageUrl);
+      /** findByIdAndRemove -- REMOVED!! */
       return Product.deleteOne({ _id: prodId, userId: req.user._id });
     })
     .then(() => {
       console.log("DESTROYED PRODUCT");
-      res.redirect("/admin/products");
+      res.status(200).json({ message: "Success!" });
     })
     .catch((err) => {
-      console.log(err);
-      const error = new Error(err);
-      error.httpStatusCode = 500;
-      return next(error);
+      res.status(500).json({ message: "Deleting product failed." });
     });
 };
